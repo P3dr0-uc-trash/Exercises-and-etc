41914a8e06a9b1f2414e3e1173e2ddca7a5d0e7f
P3dr0-uc-trash <pedrohenrique.dantas@a.ucb.br>
2024-08-28 12:01:31 -0300

    Update calculator.c

--- ativphdl/calculator.c
+++ ativphdl/calculator.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
-void calc(int a, int b, char c){
-if (c == '+')
+void calc(int* a, int* b, char* c){
+if (*c == '+')
 {
-    printf("O resultado da sua soma e: %d", (a+b));
-}    else if (c == '-')
+    printf("O resultado da sua soma e: %d", (*a+*b));
+}    else if (*c == '-')
 {
-    printf("O resultado da sua subtracao e igual a: %d", (a - b));
-}    else if (c == '*')
+    printf("O resultado da sua subtracao e igual a: %d", (*a - *b));
+}    else if (*c == '*')
 {
-    printf("O resultado da sua multiplicacao e igual a: %d", (a * b));
-}    else if (c == '/')
+    printf("O resultado da sua multiplicacao e igual a: %d", (*a * *b));
+}    else if (*c == '/')
 { 
-    printf("O resultado da sua divisao e igual a: %d", (a / b));
+    printf("O resultado da sua divisao e igual a: %d", (*a / *b));
 } else 
 {
     printf("opcao invalida");
@@ -34,7 +34,7 @@ printf("Agora me de o primeiro numero: \n");
 scanf("%d", &a);
 printf("Agora me de o segundo numero: \n");
 scanf("%d", &b);
-calc(a,b,c);
+calc(&a,&b,&c);
 } else if (resposta == 'N')
 {
 printf("ok entao, tchau\n");
