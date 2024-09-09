f87901d166638f33b00c21eecafe3f16975951b3
P3dr0-uc-trash <pedrohenrique.dantas@a.ucb.br>
2024-09-09 06:38:08 -0300

    Atualizar o funcoes em java.java

--- funcoes em java.java
+++ funcoes em java.java
@@ -3,22 +3,22 @@ package br.com.aula01pedro;
 public class SegundoProjeto {
 
 	public static void main(String[] args) {
-		String s = "Bom dia!";
+		String s = "Bom dia!";  //define a variável s como script e da a ela o valor Bom dia!
 		
-		System.out.println("Olá tudo bem?");
-		System.out.println(s);
-		System.out.println(s + "!!");
-		System.out.println(s.concat("!"));
-		s = s.toLowerCase();
-		System.out.println(s);
-		System.out.println(s.startsWith("bom"));
-		System.out.println(s.endsWith("dia!"));
-		System.out.println(s.length());
-		System.out.println(s.equals("Bom dia!"));
-		System.out.println(s.equalsIgnoreCase("Bom dia!"));
+		System.out.println("Olá tudo bem?");  //pronta a frase
+		System.out.println(s); //printa a variável s
+		System.out.println(s + "!!"); //printa o s é concatena com !!
+		System.out.println(s.concat("!")); //printa e concatenar com !
+		s = s.toLowerCase(); //define s como caixa baixa
+		System.out.println(s); //printa s
+		System.out.println(s.startsWith("bom")); //retorna true se começar com bom exatamente 
+		System.out.println(s.endsWith("dia!")); //retorna true se terminar com dia! exatamente 
+		System.out.println(s.length()); //retorna o comprimento 
+		System.out.println(s.equals("Bom dia!")); //retorna true se for exatamente igual
+		System.out.println(s.equalsIgnoreCase("Bom dia!")); //retorna true se for a mesma frase, independente da caixa
 		
-		var nome = "pedro";
-		System.out.println(nome.toUpperCase());
+		var nome = "pedro";//define a variável nome e atribui a ela o valor pedro
+		System.out.println(nome.toUpperCase()); //printa a variável nome em caixa alta
 	}
 
 }
